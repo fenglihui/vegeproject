17089ab4a56449788a90de9662d90e129e3e1c08
e <912545770@qq.com>
2018-08-02 17:21:44 +0800

    201808021721

--- WebContent/js/select.js
+++ WebContent/js/select.js
@@ -1,14 +1,24 @@
-$.fn.selectDate = function() {
-    var minYear = 2012;
-    var maxYear = (new Date).getFullYear() - 1;
-    var minManage = 1;
-    var maxManage = 50;
-    var yearSel = document.getElementById('year');
-
-    for (var y = maxYear; y >= minYear; y--) {
-        var yearOpt = document.createElement('option');
-        yearOpt.value = y
-        yearOpt.innerHTML = y + '年'
-        yearSel.appendChild(yearOpt)
-    }
-}
+$.fn.selectDate = function(){
+			var minYear = 2012;
+			var maxYear = (new Date).getFullYear()-1;
+			var yearSel = document.getElementById('year');
+			var monthSel = document.getElementById('month');
+	
+			for(var y = maxYear;y >= minYear;y--){
+				var yearOpt = document.createElement('option');
+				yearOpt.value = y
+				yearOpt.innerHTML = y+'年'
+				yearSel.appendChild(yearOpt)
+			}
+			for(var n =2;n<=12;n++){
+				var monthOpt = document.createElement('option');
+                monthOpt.value = n;
+				if( n<10){
+					monthOpt.innerHTML = '0' + n + '月';
+				}
+				else{
+					monthOpt.innerHTML = n + '月';
+				}
+				monthSel.appendChild(monthOpt);
+			}
+		}
