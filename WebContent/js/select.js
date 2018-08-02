0c7f7f8b5e0a406edd1cb82cfe713056716c1487
e <912545770@qq.com>
2018-08-02 14:04:31 +0800

    201808021404

---
+++ WebContent/js/select.js
@@ -0,0 +1,14 @@
+$.fn.selectDate = function() {
+    var minYear = 2012;
+    var maxYear = (new Date).getFullYear() - 1;
+    var minManage = 1;
+    var maxManage = 50;
+    var yearSel = document.getElementById('year');
+
+    for (var y = maxYear; y >= minYear; y--) {
+        var yearOpt = document.createElement('option');
+        yearOpt.value = y
+        yearOpt.innerHTML = y + '年'
+        yearSel.appendChild(yearOpt)
+    }
+}
